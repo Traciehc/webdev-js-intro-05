b105c7fcfa799acc4614808b8a3ff85f7809d07b
Tracie Childers <traciechilders01@gmail.com>
2024-12-26 20:46:29 -0500

    Did experiment in index.js and commented out code

--- js/index.js
+++ js/index.js
@@ -1,3 +1,34 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*
+let task = ["livePreview", "htmlConcepts", "experimentIndexJS", "commentOutCode"];
+
+console.log(task[0]); Output:  "livePreview"
+console.log(task[1]); Output:  "htmlConcepts"
+console.log(task[2]); Output:  "experimentIndexJS"
+console.log(task[3]); Output:  "commentOutCode"
+
+task[0] = "updatedPreview";
+console.log(task);  
+Output:  ["updatedPreview", "htmlConcepts", "experimentIndexJS", "commentOutCode"]
+
+let dataType = ["string", true, 10];
+
+console.log(dataType[0]);  Output:  String
+console.log(dataType[1]);  Output:  true
+console.log(dataType[2]);  Output: 10
+
+console.log(typeof dataType[0]);  Output: String
+console.log(typeof dataType[1]);  Output:  Boolean
+console.log(typeof dataType[2]);  Output:  Number
+
+dataType.shift();
+console.log(dataType);  Output:  Array [true, 10]
+
+dataType.push(20);
+console.log(dataType); Output: Array(3) [true, 10, 20] 
+
+dataType[1] = false;
+console.log(dataType);
+Output:  Array(3) [true, false, 20] */
