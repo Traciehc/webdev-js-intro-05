7a017c541413e9e3dda7f7004bec13d9acf1419b
Tracie Childers <traciechilders01@gmail.com>
2024-12-27 21:27:15 -0500

    updated README.md and completed assignment.js

--- js/assignment.js
+++ js/assignment.js
@@ -9,7 +9,8 @@ const todos = [
 ];
 
 // Declare your two immutable variables
-
+let shiftValue = "Wash the dishes";
+let popValue = "Rake the leaves";
 
 // Declaring constant variables to store references to the elements
 // that will be updated by your code.
@@ -22,43 +23,69 @@ const answerSixEl = document.getElementById("answer-six");
 const submissionBtn = document.getElementById("submission-btn");
 
 function updateAnswerOne() {
-    
+    console.log(todos); 
+    answerOneEl.innerHTML = todos.join(",");
 }
+updateAnswerOne();
 
 function updateAnswerTwo() {
-
+    console.log(todos[2]);
+    answerTwoEl.innerHTML = todos[2];
 }
+updateAnswerTwo();
 
 function removeLastValue() {
-
+    todos.pop();
 }
+removeLastValue();
 
 function removeFirstValue() {
-
+    todos.shift();
+    console.log(todos);
+    answerSixEl.innerHTML = todos.join(",");
 }
+removeFirstValue();
 
 function addShiftAndPopValues() {
-
+    todos.push(shiftValue, popValue);
+    console.log(todos); 
+    answerFiveEl.innerHTML = todos.join(",");
 }
+addShiftAndPopValues();
 
 function updateAnswerFour() {
-
+    console.log(todos);
+    answerFourEl.innerHTML = todos.join(",");
 }
+updateAnswerFour();
 
 function reverseTodoList() {
-
+    todos.reverse();
+    console.log(todos);
+    answerFourEl.innerHTML = todos.join(",");
 }
+reverseTodoList();
 
 function updateAnswerFive() {
-
+    console.log(todos);
+    answerFiveEl.innerHTML = todos.join(",");
 }
+updateAnswerFive();
 
 function updateAnswerSix() {
-
+    console.log(todos);
+    answerSixEl.innerHTML = todos.join(",");
 }
+updateAnswerSix();
 
 function render() {
-
+    updateAnswerOne();
+    updateAnswerTwo();
+    removeLastValue(); 
+    reverseTodoList(); 
+    removeFirstValue(); 
+    addShiftAndPopValues(); 
+    updateAnswerSix();
 }
 
 submissionBtn.addEventListener("click", function () {
